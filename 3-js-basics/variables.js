6fa6390869c8849abb614ea36d0b9526532c337a
arana2743 <abhisekrana777@gmail.com>
2020-09-21 22:53:00 +0530

    variables in js

---
+++ 3-js-basics/variables.js
@@ -0,0 +1,11 @@
+let name = "John";
+let age = 45;
+let isApproved = true;
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(name);
+console.log(age);
+console.log(isApproved);
+console.log(firstName);
+console.log(selectedColor);
